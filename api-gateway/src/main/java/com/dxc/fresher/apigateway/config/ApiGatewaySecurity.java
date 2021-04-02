8b59993bc800de5dc1d0a17986f29a1f172300aa
Nguyen <thanhchi.nguyen@dxc.com>
2021-04-02 17:43:37 +0700

    update

--- api-gateway/src/main/java/com/dxc/fresher/apigateway/config/ApiGatewaySecurity.java
+++ api-gateway/src/main/java/com/dxc/fresher/apigateway/config/ApiGatewaySecurity.java
@@ -82,10 +82,10 @@ public class ApiGatewaySecurity extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.GET,"/files/**").permitAll()
                 .antMatchers(HttpMethod.GET,"/users/**").permitAll()
                 // all other requests need to be authenticated
-                .anyRequest().authenticated().and().
+                .anyRequest().authenticated().and()
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
-                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         // Add a filter to validate the tokens with every request
